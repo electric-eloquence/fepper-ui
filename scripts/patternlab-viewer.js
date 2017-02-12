ca9b512580593c21ff4e9f252928eef03bc24fc0
e2tha-e <ee@electric-eloquence.io>
2017-02-12 07:37:10 -0500

    fixing styleguide.html path

--- scripts/patternlab-viewer.js
+++ scripts/patternlab-viewer.js
@@ -222,7 +222,7 @@ var urlHandler = {
     }
     
     if (name == "all") {
-      return "node_modules/fepper-ui/markup/styleguide.html";
+      return "node_modules/fepper-ui/styleguide.html";
     } else if (name == "snapshots") {
       return "snapshots/index.html";
     }
@@ -363,7 +363,7 @@ var urlHandler = {
     var iFramePath = "";
     iFramePath = this.getFileName(patternName);
     if (iFramePath === "") {
-      iFramePath = "node_modules/fepper-ui/markup/styleguide.html";
+      iFramePath = "node_modules/fepper-ui/styleguide.html";
     }
     
     var obj = JSON.stringify({ "event": "patternLab.updatePath", "path": iFramePath });
@@ -1297,7 +1297,7 @@ function receiveIframeMessage(event) {
     } else {
       
       // handle the style guide
-      path = window.location.protocol+"//"+window.location.host+window.location.pathname.replace("node_modules\/fepper-ui\/markup\/styleguide.html","")+data.path+'?'+Date.now();
+      path = window.location.protocol+"//"+window.location.host+window.location.pathname.replace("node_modules\/fepper-ui\/styleguide.html","")+data.path+'?'+Date.now();
       window.location.replace(path);
       
     }
@@ -1804,7 +1804,7 @@ window.addEventListener("message", receiveIframeMessage, false);
   // set up the defaults for the
   var baseIframePath = window.location.protocol+"//"+window.location.host+window.location.pathname.replace("index.html","");
   var patternName = ((config.defaultPattern !== undefined) && (typeof config.defaultPattern === 'string') && (config.defaultPattern.trim().length > 0)) ? config.defaultPattern : 'all';
-  var iFramePath = baseIframePath+"node_modules/fepper-ui/markup/styleguide.html?"+Date.now();
+  var iFramePath = baseIframePath+"node_modules/fepper-ui/styleguide.html?"+Date.now();
   if ((oGetVars.p !== undefined) || (oGetVars.pattern !== undefined)) {
     patternName = (oGetVars.p !== undefined) ? oGetVars.p : oGetVars.pattern;
   }
