b2c7c934c31f16d51aa912027c9c72cc41dcbf1f
e2tha-e <ee@electric-eloquence.io>
2017-04-14 22:26:14 -0400

    improved mustache browser

--- scripts/mustache-browser.js
+++ scripts/mustache-browser.js
@@ -18,12 +18,22 @@
     // Send to Fepper's Mustache browser when clicking the viewer's Mustache code.
     codeFill.addEventListener('click', function () {
       if (codeTitle.className.indexOf('sg-code-title-active') > -1) {
+        // Remove padding from viewport bottom.
+        pd.getElementById('sg-vp-wrap').style.paddingBottom = 0;
+
+        // Close nav item.
+        var sgView = pd.getElementById('sg-view');
+        sgView.style.height = 'auto';
+        sgView.classList.remove('active');
+
         var code = encodeURIComponent(this.innerHTML);
         // HTML entities for mustacheBrowser.spanTokensStrip() to work.
         code = code.replace(/><</g, '>&lt;<');
         code = code.replace(/><\/</g, '>&lt;/<');
         code = code.replace(/><!--/g, '>&lt;!--');
         var title = pd.getElementById('title').innerHTML.replace('Pattern Lab - ', '');
+
+        // Load Mustache Browser
         window.location = window.location.origin + '/mustache-browser/?title=' + title + '&code=' + code;
       }
     });
