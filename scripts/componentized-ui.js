8aa0ad3652f54eef1fbfee595d274a3b9d4785fb
e2tha-e <ee@electric-eloquence.io>
2018-07-03 06:25:04 -0400

    Squashed commit of the following:
    commit 4bf8c498f10fe8079b5fc99a529d7c4ce651aebb
    Author: e2tha-e <ee@electric-eloquence.io>
    Date:   Mon Jul 2 21:25:18 2018 -0400
    
        less specific json in html scraper
    
    commit 54346c01ce875f27e28f9d51f3d2a94be8ff799e
    Author: e2tha-e <ee@electric-eloquence.io>
    Date:   Mon Jul 2 19:50:06 2018 -0400
    
        bug fix
    
    commit 1e40d481b3df7a73ac8bc8249c6350401d5b8bfc
    Author: e2tha-e <ee@electric-eloquence.io>
    Date:   Mon Jul 2 17:23:27 2018 -0400
    
        minor linting
    
    commit a51ff4b08aa440a913377554d8b587285489da2c
    Author: e2tha-e <ee@electric-eloquence.io>
    Date:   Mon Jul 2 16:48:04 2018 -0400
    
        improved scraper messaging
    
    commit 0e17ef2f6ee794b63f0d6b4282abfa01d82b9098
    Author: e2tha-e <ee@electric-eloquence.io>
    Date:   Mon Jul 2 14:49:28 2018 -0400
    
        more client side validation on html scraper
    
    commit a0cbe713c74f51bb867c93172f30bc802dc6ca64
    Author: e2tha-e <ee@electric-eloquence.io>
    Date:   Sun Jul 1 15:53:43 2018 -0400
    
        more DOMParser usage in scraper
    
    commit 61dc0b81635cd5e1e9e5101cdbdbaeefe0497899
    Author: e2tha-e <ee@electric-eloquence.io>
    Date:   Sun Jul 1 08:41:16 2018 -0400
    
        working gatekeeping, svg-compatible scraper
    
    commit 105b40a63951804dcb841e7c6dc5b19e991b3eab
    Author: e2tha-e <ee@electric-eloquence.io>
    Date:   Sat Jun 30 06:23:38 2018 -0400
    
        lint
    
    commit ba5a385f2dbf4707f213c55c851c6cc98be6866a
    Author: e2tha-e <ee@electric-eloquence.io>
    Date:   Sat Jun 30 05:37:38 2018 -0400
    
        es5 -> es6
    
    commit 58b6d9c1114fc0c392d13d8d7c859c8ea4c4eaf1
    Author: e2tha-e <ee@electric-eloquence.io>
    Date:   Fri Jun 29 17:23:31 2018 -0400
    
        gatekeeping scraper usage

--- scripts/componentized-ui.js
+++ scripts/componentized-ui.js
@@ -1,60 +1,64 @@
-const isClient = typeof window === 'object';
-const isServer = typeof global === 'object';
+(() => {
+  'use strict';
 
-let React;
+  const isClient = typeof window === 'object';
+  const isServer = typeof global === 'object';
 
-if (isClient) {
-  React = window.React;
-}
-else if (isServer) {
-  React = require('react');
-}
+  let React;
 
-const uiCreate = function (createRenderObj) {
-  return class extends React.Component {
-    constructor(props) {
-      super(props);
+  if (isClient) {
+    React = window.React;
+  }
+  else if (isServer) {
+    React = require('react');
+  }
 
-      if (isClient) {
-        window.uiInst = this;
-        window.FEPPER_UI.reRenderFns = [];
+  function uiCreate(createRenderObj) {
+    return class extends React.Component {
+      constructor(props) {
+        super(props);
+
+        if (isClient) {
+          window.uiInst = this;
+          window.FEPPER_UI.reRenderFns = [];
+        }
+        else if (isServer) {
+          global.uiInst = this;
+        }
       }
-      else if (isServer) {
-        global.uiInst = this;
+
+      render() {
+        if (isClient) {
+          return window.componentsForClient();
+        }
+        else if (isServer) {
+          return createRenderObj();
+        }
       }
-    }
 
-    render() {
-      if (isClient) {
-        return window.componentsForClient();
+      handleEvent() {
+        delete window.componentsForClient;
+        const docHeadObj = document.getElementsByTagName('head')[0];
+        const dynamicScriptId = 'components-for-client';
+        const dynamicScriptOld = document.getElementById(dynamicScriptId);
+        const dynamicScriptNew = document.createElement('script');
+
+        dynamicScriptNew.id = dynamicScriptId;
+        dynamicScriptNew.innerHTML = dynamicScriptOld.innerHTML;
+        docHeadObj.removeChild(dynamicScriptOld);
+        docHeadObj.appendChild(dynamicScriptNew);
+
+        while (window.FEPPER_UI.reRenderFns.length) {
+          window.FEPPER_UI.reRenderFns.shift()();
+        }
       }
-      else if (isServer) {
-        return createRenderObj();
-      }
-    }
+    };
+  }
 
-    handleEvent() {
-      delete window.componentsForClient;
-      const docHeadObj = document.getElementsByTagName('head')[0];
-      const dynamicScriptId = 'components-for-client';
-      const dynamicScriptOld = document.getElementById(dynamicScriptId);
-      const dynamicScriptNew = document.createElement('script');
-
-      dynamicScriptNew.id = dynamicScriptId;
-      dynamicScriptNew.innerHTML = dynamicScriptOld.innerHTML;
-      docHeadObj.removeChild(dynamicScriptOld);
-      docHeadObj.appendChild(dynamicScriptNew);
-
-      while (window.FEPPER_UI.reRenderFns.length) {
-        window.FEPPER_UI.reRenderFns.shift()();
-      }
-    }
-  };
-}
-
-if (isClient) {
-  window.FEPPER_UI.uiCreate = uiCreate;
-}
-else if (isServer) {
-  module.exports = uiCreate;
-}
+  if (isClient) {
+    window.FEPPER_UI.uiCreate = uiCreate;
+  }
+  else if (isServer) {
+    module.exports = uiCreate;
+  }
+})();
