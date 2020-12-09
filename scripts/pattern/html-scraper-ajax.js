662c7ca5a19ed29055f067fcbfb36167d3cfe344
e2tha-e <ee@electric-eloquence.io>
2020-12-09 15:50:11 -0500

    scraper allows js to run on content to be scraped

--- scripts/pattern/html-scraper-ajax.js
+++ scripts/pattern/html-scraper-ajax.js
@@ -1,91 +1,18 @@
+// DEPRECATED.
+// All operations herein will be moved to html-scraper.js.
 ((d) => {
   'use strict'; // eslint-disable-line strict
 
-  function nodeListToJson(nodeItem, jsonToRecurse) {
-    switch (nodeItem.nodeType) {
-      case 1:
-        jsonToRecurse.node = 'element';
-
-        if (typeof nodeItem.nodeName === 'string') {
-          jsonToRecurse.tag = nodeItem.nodeName.toLowerCase();
-        }
-
-        if (nodeItem.attributes.length) {
-          jsonToRecurse.attr = {};
-
-          for (let i = 0, l = nodeItem.attributes.length; i < l; i++) {
-            const attribute = nodeItem.attributes[i];
-            jsonToRecurse.attr[attribute.name] = attribute.value;
-          }
-        }
-
-        break;
-
-      case 3:
-        jsonToRecurse.node = 'text';
-        jsonToRecurse.text = nodeItem.textContent;
-
-        break;
-
-      case 8:
-        jsonToRecurse.node = 'comment';
-        jsonToRecurse.text = nodeItem.textContent;
-
-        break;
-
-      default:
-        jsonToRecurse.node = '';
-    }
-
-    if (nodeItem.childNodes.length) {
-      jsonToRecurse.child = [];
-
-      for (let i = 0, l = nodeItem.childNodes.length; i < l; i++) {
-        jsonToRecurse.child.push({});
-        nodeListToJson(nodeItem.childNodes[i], jsonToRecurse.child[i]);
-      }
-    }
+  function appendMain(main) {
+    d.body.appendChild(main);
   }
 
-  /**
-   * Validate syntax of Target Selector input.
-   *
-   * @param {string} selectorRaw_ - CSS selector plus optional array index.
-   * @returns {string|null} The selector name or null if invalid.
-   */
-  function selectorValidateAndParse(selectorRaw_) {
-    const selectorRaw = selectorRaw_.trim();
-    const bracketOpenPos = selectorRaw.indexOf('[');
-    const bracketClosePos = selectorRaw.indexOf(']');
+  function createMain() {
+    const main = d.createElement('main');
 
-    let indexStr;
-    let name = selectorRaw;
+    main.setAttribute('id', 'fepper-html-scraper');
 
-    // Slice selectorRaw to extract name and indexStr if submitted.
-    if (bracketOpenPos > -1) {
-      if (bracketClosePos === selectorRaw.length - 1) {
-        indexStr = selectorRaw.slice(bracketOpenPos + 1, bracketClosePos);
-        name = selectorRaw.slice(0, bracketOpenPos);
-      }
-      else {
-        return null;
-      }
-    }
-
-    // Validate that name is a css selector.
-    // eslint-disable-next-line no-useless-escape
-    if (!/^(#|\.)?[_a-z][\w#\-\.]*$/i.test(name)) {
-      return null;
-    }
-
-    // If indexStr if submitted, validate it is an integer.
-    if (indexStr) {
-      if (!/^\d+$/.test(indexStr)) {
-        return null;
-      }
-    }
-
-    return name;
+    return main;
   }
 
   /* MAIN EXECUTION */
@@ -96,142 +23,80 @@
   const emptyFrag = new DocumentFragment();
 
   // First, make sure the HTML scraper is being requested from the same machine that's running the Express app.
-  new Promise(
-    (resolve, reject) => {
-      const xhr = new XMLHttpRequest();
+  fetch(baseUrl + '/gatekeeper')
+    .then(() => fetch(baseUrl + '/html-scraper-xhr' + window.location.search))
+    .then((response) => response.text())
+    .then((responseText) => {
+      return new Promise((resolve) => {
+        // Parse xhr.responseText into DOM object.
+        const parser = new DOMParser();
+        const doc = parser.parseFromString(responseText, 'text/html');
+        let main = d.getElementById('main');
 
-      xhr.open('GET', baseUrl + '/gatekeeper', true);
-      xhr.onload = () => {
-        if (xhr.responseText) {
-          resolve();
-        }
-        else {
-          reject();
-        }
-      };
-      xhr.send();
-    })
-    .then(() => {
-      return new Promise(
-        (resolve) => {
-          const xhr = new XMLHttpRequest();
+        if (main) {
+          // DEPRECATED as of 2020-12-08. To be removed.
+          const heading = doc.getElementById('scraper-heading') || emptyFrag;
+          const helpText = doc.getElementById('help-text') || emptyFrag;
+          const loadAnim = doc.getElementById('load-anim') || emptyFrag;
+          const message = doc.getElementById('message') || emptyFrag;
+          // Get last form on page. Older Fepper versions didn't identify it by name.
+          const targeter = doc.forms[doc.forms.length - 1]; // Allow fully logged failure if this returns null.
 
-          xhr.open('GET', baseUrl + '/html-scraper-xhr' + window.location.search, true);
-          xhr.onload = () => {
-            // Parse xhr.responseText into DOM object.
-            const parser = new DOMParser();
-            const doc = parser.parseFromString(xhr.responseText, 'text/html');
-            const heading = doc.getElementById('scraper-heading') || emptyFrag;
-            const helpText = doc.getElementById('help-text') || emptyFrag;
-            const loadAnim = doc.getElementById('load-anim') || emptyFrag;
-            const message = doc.getElementById('message') || emptyFrag;
-            // Get last form on page. Older Fepper versions didn't identify it by name.
-            const targeter = doc.forms[doc.forms.length - 1]; // Allow fully logged failure if this returns null.
-
-            // Write out main content.
-            const main = d.getElementsByTagName('main')[0];
-
-            if (!main.getElementsByClassName('message').length) {
-              main.appendChild(message);
-            }
-
-            main.appendChild(loadAnim);
-
-            if (!main.getElementsByClassName('scraper-heading').length) {
-              main.appendChild(heading);
-            }
-
-            main.appendChild(targeter);
-            main.appendChild(helpText);
-
-            // Insert new script element such that it fires on load.
-            const node4insert = d.getElementById('help-text');
-
-            if (node4insert) {
-              const script2insert = d.createElement('script');
-              script2insert.src = '../../node_modules/fepper-ui/scripts/pattern/html-scraper-dhtml.js';
-
-              node4insert.parentNode.insertBefore(script2insert, node4insert);
-            }
-
-            resolve();
-          };
-          xhr.send();
-        });
-    })
-    .then(() => {
-      // Get last form on page. Older Fepper versions didn't identify it by name.
-      const targeter = d.forms[d.forms.length - 1];
-
-      targeter.addEventListener(
-        'submit',
-        function (e) {
-          e.preventDefault();
-
-          const loadAnim = d.getElementById('load-anim') || {style: {}};
-          const message = d.getElementById('message'); // Allow fully logged failure if this returns null.
-          const xhr = new XMLHttpRequest();
-          const selectorRaw = targeter.selector.value;
-          let url = targeter.url.value;
-
-          if (url[0] === '/') {
-            url = baseUrl + url;
+          // Write out main content.
+          if (!main.getElementsByClassName('message').length) {
+            main.appendChild(message);
           }
 
-          xhr.open('GET', baseUrl + '/html-scraper-xhr/cors?url=' + encodeURIComponent(url), true);
-          xhr.onload = () => {
-            // Fail and return on error.
-            if (xhr.readyState === 4 && xhr.status === 200) {
-              const selectorName = selectorValidateAndParse(selectorRaw);
+          main.appendChild(loadAnim);
 
-              if (!selectorName) {
-                message.className = 'message error';
-                message.innerHTML = 'ERROR! Please enter correctly syntaxed selector.';
-                d.body.scrollTop = d.documentElement.scrollTop = 0;
-                loadAnim.style.display = 'none';
+          if (!main.getElementsByClassName('scraper-heading').length) {
+            main.appendChild(heading);
+          }
 
-                return;
-              }
+          main.appendChild(targeter);
+          main.appendChild(helpText);
 
-              // Parse xhr.responseText as DOM. Create an object consumable by the html2json library.
-              const parser = new DOMParser();
-              const doc = parser.parseFromString(xhr.responseText, 'text/html');
-              const selection = doc ? doc.querySelectorAll(selectorName) : [];
-              const html2json = {node: 'root', child: []};
+          // Insert new script element such that it fires on load.
+          const node4insert = d.getElementById('help-text');
 
-              for (let i = 0, l = selection.length; i < l; i++) {
-                html2json.child.push({});
-                nodeListToJson(selection[i], html2json.child[i]);
-              }
+          if (node4insert) {
+            const script2insert = d.createElement('script');
+            script2insert.src = '../../node_modules/fepper-ui/scripts/pattern/html-scraper-dhtml.js';
 
-              message.className = 'message';
-              message.innerHTML = '';
-
-              targeter.html2json.value = JSON.stringify(html2json);
-              targeter.submit();
-            }
-            else {
-              message.className = 'message error';
-              message.innerHTML = 'ERROR! Please enter a valid, reachable URL.';
-              d.body.scrollTop = d.documentElement.scrollTop = 0;
-              loadAnim.style.display = 'none';
-            }
-          };
-          xhr.send();
+            node4insert.parentNode.insertBefore(script2insert, node4insert);
+          }
         }
-      );
+        else {
+          const scraperDhtml = d.createElement('script');
+          scraperDhtml.src = '../../node_modules/fepper-ui/scripts/pattern/html-scraper-dhtml.js';
+          main = createMain();
+          main.innerHTML = doc.body.innerHTML;
+
+          appendMain(main);
+          d.body.appendChild(scraperDhtml);
+        }
+
+        resolve();
+      });
     })
     .catch(() => {
-      const xhr = new XMLHttpRequest();
+      return fetch(baseUrl + '/html-scraper-xhr/forbidden' + window.location.search)
+        .then((response) => response.text())
+        .then((responseText) => {
+          const parser = new DOMParser();
+          const doc = parser.parseFromString(responseText, 'text/html');
+          const forbidden = doc.getElementById('forbidden');
+          let main = d.getElementById('main');
 
-      xhr.open('GET', baseUrl + '/html-scraper-xhr/forbidden' + window.location.search, true);
-      xhr.onload = () => {
-        const parser = new DOMParser();
-        const doc = parser.parseFromString(xhr.responseText, 'text/html');
-        const forbidden = doc.getElementById('forbidden');
-        const main = d.getElementsByTagName('main')[0];
-        main.innerHTML = forbidden.outerHTML;
-      };
-      xhr.send();
+          if (main) {
+            main.innerHTML = forbidden.outerHTML;
+          }
+          else {
+            main = createMain();
+            main.innerHTML = forbidden.outerHTML;
+
+            appendMain(main);
+          }
+        });
     });
 })(document);
--- scripts/pattern/html-scraper-dhtml.js
+++ scripts/pattern/html-scraper-dhtml.js
@@ -1,31 +1,169 @@
 ((d) => {
   'use strict'; // eslint-disable-line strict
 
+  const baseUrl = window.location.protocol + '//' + window.location.host;
+  const message = d.getElementById('message') || {style: {}};
+  const stage = d.getElementById('scraper__stage');
   // Targeter is the last form on the HTML Scraper page. Older Fepper versions didn't identify it by name.
-  const targeter = d.forms[d.forms.length - 1];
+  // DEPRECATED as of 2020-12-08: anonymous form.
+  const targeter = d.forms.targeter || d.forms[d.forms.length - 1];
 
-  targeter.addEventListener('submit', () => {
-    // First show loading animation.
-    const loadAnim = d.getElementById('load-anim') || {style: {}};
-    loadAnim.style.display = 'block';
+  function fetchCors(url_) {
+    let url;
 
-    // Then, append search params to target.
-    const baseUrl = window.location.protocol + '//' + window.location.host;
-    let url = targeter.url.value;
-    targeter.action = '/html-scraper?url=';
-
-    if (url[0] === '/') {
-      url = baseUrl + url;
+    if (url_[0] === '/') {
+      url = baseUrl + url_;
+    }
+    else {
+      url = url_;
     }
 
-    targeter.action += encodeURIComponent(url);
-    targeter.action += '&selector=';
-    targeter.action += encodeURIComponent(targeter.selector.value);
+    return fetch(baseUrl + '/html-scraper-xhr/cors?url=' + encodeURIComponent(url))
+      .then((response) => {
+        return new Promise((resolve, reject) => {
+          if (response.ok) {
+            stage.src = baseUrl + '/html-scraper-xhr/cors?url=' + encodeURIComponent(url);
+
+            resolve();
+          }
+          else {
+            message.className = 'message error';
+            message.innerHTML = 'ERROR! Please enter a valid, reachable URL.';
+            d.body.scrollTop = d.documentElement.scrollTop = 0;
+
+            reject(response.statusText);
+          }
+        });
+      })
+      .catch((err) => void console.error(err)); // eslint-disable-line no-console
+  }
+
+  function nodeListToJson(nodeItem, jsonToRecurse) {
+    switch (nodeItem.nodeType) {
+      case 1:
+        jsonToRecurse.node = 'element';
+
+        if (typeof nodeItem.nodeName === 'string') {
+          jsonToRecurse.tag = nodeItem.nodeName.toLowerCase();
+        }
+
+        if (nodeItem.attributes.length) {
+          jsonToRecurse.attr = {};
+
+          for (let i = 0, l = nodeItem.attributes.length; i < l; i++) {
+            const attribute = nodeItem.attributes[i];
+            jsonToRecurse.attr[attribute.name] = attribute.value;
+          }
+        }
+
+        break;
+
+      case 3:
+        jsonToRecurse.node = 'text';
+        jsonToRecurse.text = nodeItem.textContent;
+
+        break;
+
+      case 8:
+        jsonToRecurse.node = 'comment';
+        jsonToRecurse.text = nodeItem.textContent;
+
+        break;
+
+      default:
+        jsonToRecurse.node = '';
+    }
+
+    if (nodeItem.childNodes.length) {
+      jsonToRecurse.child = [];
+
+      for (let i = 0, l = nodeItem.childNodes.length; i < l; i++) {
+        jsonToRecurse.child.push({});
+        nodeListToJson(nodeItem.childNodes[i], jsonToRecurse.child[i]);
+      }
+    }
+  }
+
+  function validateAndParseSelector(selectorRaw) {
+    const selectorSplit = selectorRaw.trim().split('[');
+    const selector = selectorSplit[0];
+    let index;
+    let retVal;
+
+    // Validate.
+    // eslint-disable-next-line no-useless-escape
+    if (!/^(#|\.)?[_a-z][\w#\-\.]*$/i.test(selector)) {
+      retVal = null;
+    }
+
+    if (selectorSplit[1]) {
+      if (/^\d+\]$/.test(selectorSplit[1])) {
+        index = selectorSplit[1].slice(0, -1);
+      }
+      else {
+        retVal = null;
+      }
+    }
+
+    if (retVal === null) {
+      message.className = 'message error';
+      message.innerHTML = 'ERROR! Please enter a correctly syntaxed selector.';
+      d.body.scrollTop = d.documentElement.scrollTop = 0;
+    }
+    else {
+      message.className = 'message';
+      message.innerHTML = '';
+      targeter.selector.value = selector;
+      targeter.index.value = index;
+      retVal = {selector, index};
+    }
+
+    return retVal;
+  }
+
+  targeter.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const validatedSelector = validateAndParseSelector(targeter.selectorRaw.value);
+
+    if (validatedSelector) {
+      const {selector, index} = validatedSelector;
+
+      stage.setAttribute('data-selector', selector);
+      stage.setAttribute('data-index', index || '');
+
+      fetchCors(targeter.url.value);
+    }
+  });
+
+  stage.addEventListener('load', () => {
+    const innerHTML = stage.contentWindow.document.body.innerHTML;
+
+    if (innerHTML) {
+      // Parse xhr.responseText as DOM. Create an object consumable by the html2json library.
+      const parser = new DOMParser();
+      const doc = parser.parseFromString(innerHTML, 'text/html');
+      const selector = stage.getAttribute('data-selector');
+      const selection = doc ? doc.querySelectorAll(selector) : [];
+      const html2json = {node: 'root', child: []};
+
+      for (let i = 0, l = selection.length; i < l; i++) {
+        html2json.child.push({});
+        nodeListToJson(selection[i], html2json.child[i]);
+      }
+
+      message.className = 'message';
+      message.innerHTML = '';
+
+      targeter.html2json.value = JSON.stringify(html2json);
+      targeter.submit();
+    }
   });
 
   // Show/hide help text.
-  const helpButton = d.getElementById('help-button');
-  const hideButton = d.getElementById('hide-button');
+  // DEPRECATED as of 2020-12-08: #help-button and #hide-button selectors.
+  const helpButton = d.getElementById('help-button') || d.getElementById('help-show');
+  const hideButton = d.getElementById('hide-button') || d.getElementById('help-hide');
   let helpText;
 
   if (helpButton) {
@@ -61,7 +199,6 @@
       ) {
         e.preventDefault();
 
-        const message = d.getElementById('message') || {style: {}};
         message.className = 'message error';
         message.innerHTML = 'ERROR! Please enter a valid filename.';
         d.body.scrollTop = d.documentElement.scrollTop = 0;
