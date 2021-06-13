a142df28a36c93b2fc1cdf60563dbef4a551ec1d
electric-el <electric-el@electric-eloquence.io>
2021-06-13 09:05:37 -0400

    updated xhr mock for unit tests

--- test/mocks/XMLHttpRequest.js
+++ test/mocks/XMLHttpRequest.js
@@ -10,7 +10,10 @@ global.XMLHttpRequest = class {
   send() {
     let status;
 
-    if (this.responseURL.startsWith('patterns/01-compounds-block/01-compounds-block.md')) {
+    if (this.responseURL.startsWith('/patterns/00-elements-paragraph/00-elements-paragraph.md')) {
+      status = 200;
+    }
+    if (this.responseURL.startsWith('/patterns/01-compounds-block/01-compounds-block.md')) {
       status = 200;
     }
 
